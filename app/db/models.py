c3af04d7c5eb2b5754937c3b7139bcee56508b4b
Joshua Thijssen <jthijssen@noxlogic.nl>
2024-06-19 10:47:48 +0200

    Added context system for sessions

--- app/db/models.py
+++ app/db/models.py
@@ -1,30 +1,6 @@
 from __future__ import annotations
-from typing import List
-from pydantic import BaseModel
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
     pass
-
-
-class Example(Base):
-    __tablename__ = "examples"
-
-    id: Mapped[int] = mapped_column("id", Integer, primary_key=True)
-    name: Mapped[str] = mapped_column("name", String(100), nullable=False)
-
-    def __repr__(self) -> str:
-        return f"<Example(id={self.id}, name={self.name})>"
-
-
-class Meta(BaseModel):
-    limit: int
-    offset: int
-    total: int
-
-
-class ExampleResponse(BaseModel):
-    meta: Meta
-    items: List[str]
