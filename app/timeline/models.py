0490e0dfe7ffd05940ddcee78e41a790203b89ed
Joshua Thijssen <jthijssen@noxlogic.nl>
2024-07-11 11:14:59 +0200

    Renamed medmij to more generic provider-id

--- app/timeline/models.py
+++ app/timeline/models.py
@@ -7,7 +7,7 @@ from app.api.metadata.models import Metadata
 
 class TimelineEntry(BaseModel):
     healthcare_provider_name: str
-    healthcare_provider_medmij_id: str
+    healthcare_provider_id: str
     error: bool = False
     error_msg: Optional[str] = None
     items: Optional[dict[str, Metadata]] = None
