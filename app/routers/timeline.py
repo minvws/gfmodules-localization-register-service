f5c0a8fc25ebfdc25041ee509275f0be680fc5b3
Fouad Sfarijlani <fouad.sfarijlani@hotmail.com>
2024-10-09 09:29:05 +0200

    docs: Request timeline interface documentation
    added response type to timeline interface

--- app/routers/timeline.py
+++ app/routers/timeline.py
@@ -20,36 +20,45 @@ class TimelineRequest(BaseModel):
     """
     Request model for timeline
     """
+
     pseudonym: str
     data_domain: str
 
 
-@router.post("/timeline",
-            summary="Search for all timeline events",
-            tags=["timeline_group"]
-            )
+@router.post(
+    "/timeline", summary="Search for all timeline events", tags=["timeline_group"]
+)
 def post_timeline(
     req: TimelineRequest,
-    timeline_service: TimelineService = Depends(container.get_timeline_service)
+    timeline_service: TimelineService = Depends(container.get_timeline_service),
 ) -> Any:
+    print(req)
     span = trace.get_current_span()
-    span.update_name(f"POST /timeline pseudonym={req.pseudonym} data_domain={req.data_domain}")
+    span.update_name(
+        f"POST /timeline pseudonym={req.pseudonym} data_domain={req.data_domain}"
+    )
 
     try:
         pseudonym = Pseudonym(req.pseudonym)
     except ValueError:
-        raise FHIRException(status_code=400, severity="error", code="invalid", msg="Invalid pseudonym")
+        raise FHIRException(
+            status_code=400, severity="error", code="invalid", msg="Invalid pseudonym"
+        )
 
     data_domain = DataDomain.from_str(req.data_domain)
     if data_domain is None:
-        raise FHIRException(status_code=400, severity="error", code="invalid", msg="Invalid data domain")
+        raise FHIRException(
+            status_code=400, severity="error", code="invalid", msg="Invalid data domain"
+        )
 
     try:
         with get_tracer().start_as_current_span("retrieve_timeline") as tl_span:
             timeline = timeline_service.retrieve(pseudonym, data_domain)
             tl_span.add_event("timeline_retrieved")
     except TimelineError as e:
-        raise FHIRException(status_code=400, severity="error", code="invalid", msg=e.message)
+        raise FHIRException(
+            status_code=400, severity="error", code="invalid", msg=e.message
+        )
     ret_value = timeline.dict()
     span.set_attribute("data.number_of_bundles", timeline.total)
     return ret_value
